6b05734be1c7c04c676b1dbaa11ba53fe40e7cfe
ganhuanhui <ganhuanhui@kt720.com>
2019-12-25 15:04:55 +0800

    提交apk

--- app/src/main/java/com/test/commentdialog/MainActivity.java
+++ app/src/main/java/com/test/commentdialog/MainActivity.java
@@ -1,9 +1,6 @@
 package com.test.commentdialog;
 
 import android.view.View;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
 import com.test.commentdialog.multi.CommentMultiActivity;
 import com.test.commentdialog.single.CommentSingleActivity;
 
@@ -16,8 +13,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     void initView() {
-        ImageView iv = findViewById(R.id.iv);
-        Glide.with(this).load("https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1918451189,3095768332&fm=26&gp=0.jpg").into(iv);
     }
 
     @Override
